76fb17ec78d5648c1292eb5879d3a837de907f77
Kami <josephkiarie561@gmail.com>
2025-10-28 12:58:08 +0300

    load cached resources

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 import joblib
 import matplotlib.pyplot as plt
+from xgboost import XGBClassifier
 
 # title and description
 st.set_page_config(
@@ -21,3 +22,20 @@ st.markdown(
     The model uses soil nutrients (NPK), pH, rainfall, temperature, and humidity to make predictions.
     """
 )
+
+# resource loader
+@st.cache_resource
+def load_resources():
+    # loading encoder
+    encoder = joblib.load("app/../resources/encoder.pkl")
+    
+    # laoding scaler
+    scaler = joblib.load("app/../resources/scaler.pkl")
+
+    # loading model
+    model = XGBClassifier()
+    model.load_model("app/../resources/crop_model.json")
+
+    return encoder, scaler, model
+
+encoder, scaler, model = load_resources()
